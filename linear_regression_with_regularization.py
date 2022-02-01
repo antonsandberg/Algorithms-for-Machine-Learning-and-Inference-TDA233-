df65140919e7304514620e78f190e8b52f573b62
antonsandberg <61949298+antonsandberg@users.noreply.github.com>
2022-02-01 20:39:23 +0100

    Add files via upload

--- linear_regression_with_regularization.py
+++ linear_regression_with_regularization.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 
 def fit_linear_with_regularization(X, y, alpha):
+
+    # w = (X'X + a)^(-1)(X'y)
     w = np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X) + alpha),
                   np.matmul(np.transpose(X), y))
     return w
@@ -15,13 +17,33 @@ def predict(X, w):
     return y_prediction
 
 
-def plot_prediction(X_test, y_test, y_pred):
-    plt.scatter(X_test[:, 0], y_test, label='Real data')
-    plt.scatter(X_test[:, 0], y_pred, label='Prediction')
+# Just for visualizing the data for our own sanity
+def plot_training(X_train, y_train):
+
+    plt.scatter(X_train[:, 0], y_train, label='Real data')
+    plt.title('Visualization of the training data')
     plt.xlabel('First attribute')
     plt.ylabel('Diabetes data')
-    plt.title('Linear regression with regularization')
     plt.legend()
+    plt.show()
+
+
+def plot_prediction(X_test, y_test, y_pred):
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle('Linear regression with regularization')
+    ax1.plot(range(len(y_test)), y_test, label='Real data')
+    ax1.plot(range(len(y_pred)), y_pred, label='Prediction')
+    ax1.set(xlabel='First attribute', ylabel='Diabetes data')
+    ax1.set_title('With num data')
+    ax1.legend()
+
+    ax2.plot(X_test[:, 0], y_test, '*', label='Real data')
+    ax2.plot(X_test[:, 0], y_pred, '*', label='Prediction')
+    ax2.set(xlabel='Range', ylabel='Diabetes data')
+    ax2.set_title('With diabetes data')
+    ax2.legend()
+    plt.tight_layout()
     plt.show()
 
     # Compute the mean squared error
@@ -31,7 +53,6 @@ def plot_prediction(X_test, y_test, y_pred):
 
 # Load the diabetes dataset, X input, y output
 X, y = datasets.load_diabetes(return_X_y=True)
-
 # Split the dataset into training and test set
 num_test_elements = 20
 
@@ -47,6 +68,8 @@ alpha = 0.01
 w = fit_linear_with_regularization(X_train, y_train, alpha)
 # Make predictions using the testing set X_test
 y_pred = predict(X_test, w)
+y_pred = y_pred
 error = plot_prediction(X_test, y_test, y_pred)
-
 print(f'Mean Squared error is: {error}')
+plot_training(X_train, y_train)
+
