f2d498f05a21a8cc0e6b4de84ab55b2fb54d461c
antonsandberg <61949298+antonsandberg@users.noreply.github.com>
2022-02-02 09:54:33 +0100

    Add files via upload

--- linear_regression_with_regularization.py
+++ linear_regression_with_regularization.py
@@ -5,9 +5,9 @@ import matplotlib.pyplot as plt
 
 
 def fit_linear_with_regularization(X, y, alpha):
-
-    # w = (X'X + a)^(-1)(X'y)
-    w = np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X) + alpha),
+    N = len(y)
+    # w = (X'X + aI)^(-1)(X'y)
+    w = np.matmul(np.linalg.inv(np.matmul(np.transpose(X), X) + np.identity(np.shape(X)[1])*alpha),
                   np.matmul(np.transpose(X), y))
     return w
 
@@ -34,20 +34,21 @@ def plot_prediction(X_test, y_test, y_pred):
     fig.suptitle('Linear regression with regularization')
     ax1.plot(range(len(y_test)), y_test, label='Real data')
     ax1.plot(range(len(y_pred)), y_pred, label='Prediction')
-    ax1.set(xlabel='First attribute', ylabel='Diabetes data')
+    ax1.set(xlabel='Range(len)', ylabel='Diabetes data')
     ax1.set_title('With num data')
     ax1.legend()
 
     ax2.plot(X_test[:, 0], y_test, '*', label='Real data')
     ax2.plot(X_test[:, 0], y_pred, '*', label='Prediction')
-    ax2.set(xlabel='Range', ylabel='Diabetes data')
+    ax2.set(xlabel='First attribute', ylabel='Diabetes data')
     ax2.set_title('With diabetes data')
     ax2.legend()
     plt.tight_layout()
     plt.show()
 
     # Compute the mean squared error
-    mse = mean_squared_error(y_test, y_pred)
+    # mse = mean_squared_error(y_test, y_pred)
+    mse = 1/len(y_test)*np.sum((y_test - y_pred)**2)
     return mse
 
 
@@ -63,7 +64,7 @@ y_train = y[:-num_test_elements]
 y_test = y[-num_test_elements:]
 
 # Set alpha
-alpha = 0.01
+alpha = 0.0000001
 # Train using linear regression with regularization and find optimal model
 w = fit_linear_with_regularization(X_train, y_train, alpha)
 # Make predictions using the testing set X_test
